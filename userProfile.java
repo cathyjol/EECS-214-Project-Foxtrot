5e5d8bb8e86e0bb21c98ad0b9a5fe626f1d2c6df
cathyjol <cathylopez2015@u.northwestern.edu>
2014-05-06 17:52:06 -0500

    Update userProfile.java

--- userProfile.java
+++ userProfile.java
@@ -4,8 +4,10 @@
  * Movie database
  */
 
+package foxtrot;
+
 public class userProfile {
-    private String username; // Name of user
+    private final String username; // Name of user
     public node reviews; // First review created by user
     public node mostRecentlyWatched; // Last movie reviewed by user
     public int numReviews; // Number of reviews created by user
@@ -18,5 +20,20 @@ public class userProfile {
     }
     
     // Creates link within profile to new movie review created by user
-    public void newReview();
+    public void newReview(String title) {
+        /*
+         * Interactive method that prompts user for appropriate inputs
+         * to instantiate a new review object and then proceeds to call appropriate set functions
+         *
+         * Carries out following tasks: Increments numReviews by 1
+         *                              Calls review(title, this.username) to
+         *                                  set title and critic variables of constructor
+         *                              Prompts for genres and calls set method
+         *                              Prompts for rating and calls set method
+         *                              Prompts for yes/no recommend? and calls set method
+         *
+         *                              mostRecentlyWatched.info = new movie object of review
+         *                              reviews.link references first review made by user
+         */
+    }
 }
