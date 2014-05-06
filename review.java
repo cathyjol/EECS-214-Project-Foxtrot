5dfb4264caf5eefef82757a328f417a4c4b5533e
cathyjol <cathylopez2015@u.northwestern.edu>
2014-05-06 17:51:42 -0500

    Update review.java

--- review.java
+++ review.java
@@ -4,11 +4,54 @@
  * Movie database
  */
 
-public class review implements MovieInterface {
+package foxtrot;
+
+public class review {
     public String title; // Title of the movie
-    public int rating; // User rating
-    public genres genreList[3]; // List (maximum of 3) of movie's genres
+    private int rating; // User rating
+    public String[] genreList; // List (maximum of 3) of movie's genres
+    public userProfile critic;
+    private int recommends;
     protected node location; // Node containing movie reviewed, if already in database
     
+    public review(String u, userProfile edit) {
+        title = u;
+        critic = edit;
+        genreList = new String[3];
+    }
     
+    // Sets review rating
+    public void setRating(int rate) {
+        rating = rate;
+    }
+    
+    // Gets review rating
+    public int getRating() {
+        return rating;
+    }
+    
+    // Sets review genres
+    public void setGenres(String first, String second, String third) {
+        genreList[0] = first;
+        genreList[1] = second;
+        genreList[2] = third;
+    }
+
+    // Sets review recommendation
+    public void setRecommends(int ok) {
+        recommends = ok;
+    }
+    
+    // Gets review recommendation
+    public int getRecommends() {
+        return recommends;
+    }
+    
+    public boolean movieObject(String k) {
+        /* 
+         * Iterates through movie database list to compare all titles with string k
+         * If movie already exists, return true; otherwise, return false
+         */
+        return false;
+    }
 }
