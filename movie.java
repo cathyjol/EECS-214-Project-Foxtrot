eeabdc53c8ce7f190b5769d748c429d261256f77
Yang223 <gy446203531@gmail.com>
2014-05-22 16:18:05 -0500

    Update movie.java

--- movie.java
+++ movie.java
@@ -3,8 +3,7 @@
  * EECS 214 Project Foxtrot
  * Movie database
  */
-
-package foxtrot;
+package finalproject;
 
 public class movie {
     public String title; // Title of the movie
@@ -13,9 +12,14 @@ public class movie {
     public String[] genreList; // List (maximum of 3) of movie's genres
     
     // Constructor method
-    public movie(String t, int r) {
-        title = null;
-        rating = r;
+    public movie(String t, double r) {
+        title = t;
+        if(r > 5)
+            rating = 5;
+        else if(r < 0)
+            rating = 0;
+        else
+            rating = r;
         numReviews = 1;
         genreList = new String[3];
     }
@@ -33,22 +37,34 @@ public class movie {
         return output;
     }
     
+    // Initialize genres
+    public void setGenre(String genre1, String genre2, String genre3) {
+        genreList[0] = genre1;
+        genreList[1] = genre2;
+        genreList[2] = genre3;
+    }
+        
     // Iterates through movie's array of genres
     // and adds reference to itself within appropriate genres
-    public void addToGenre() {
+    public void addToGenre(genres genre) {
         /*
          * Accessing this.genreList[], iterates through pre-determined genre titles
          * and if (genre.title).equals(this.genreList[i]) (i.e. a genre within the movie's genre array
          * matches a genre within our pre-determined set, the genres.add(this) will be invoked
          */
-        
+        for(int i=0;i<3;i++)
+        {
+            if(this.genreList[i].equals(genre.title))
+                genre.add(this);
+        }
     }
     
     public void setRating(int rating) {
         /* 
          *
          */
-         this.rating = rating;
+         this.rating = (this.rating * numReviews + rating) / (numReviews + 1);
+         numReviews += 1;
     }
     
     // Iterates through list of movies within a genre to compare this movie's rating,
@@ -59,6 +75,21 @@ public class movie {
          * If this.rating < opponent.rating, position this movie after opponent in list
          * If this.rating == opponent.rating, position this movie before opponent in list
          */
-        return 0;
+        if(this.rating > opponent.rating)
+            return 1;
+        if(this.rating < opponent.rating)
+            return -1;
+        else
+            return 0;
+    }
+    
+    // 
+    public int compareToTitle(movie opponent) {
+        if(this.title.compareToIgnoreCase(opponent.title) < 0)
+            return -1;
+        else if(this.title.compareToIgnoreCase(opponent.title) > 0)
+            return 1;
+        else 
+            return 0;
     }
 }
